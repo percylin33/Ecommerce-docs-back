e4dea7727e0d1546e330b52f6a40edc2203398c0
percy <p3r.valderrama@gmail.com>
2024-10-04 22:21:06 -0500

    feat-extructura

--- src/main/java/com/carpetadigital/ecommerce/products/controller/ProductsController.java
+++ src/main/java/com/carpetadigital/ecommerce/products/controller/ProductsController.java
@@ -1,4 +1,4 @@
-package com.carpetadigital.ecommerce.Demo;
+package com.carpetadigital.ecommerce.products.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
-public class DemoController {
+public class ProductsController {
     @PostMapping(value = "/demo")
     public String welcome(){
         return "welcome from secure endpoint";
